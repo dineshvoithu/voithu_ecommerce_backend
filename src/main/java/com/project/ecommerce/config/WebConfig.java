bff59c974f02ef6566abd9d7dc1640271a48bb88
dineshvoithu <dineshkumarvoithu@gmail.com>
2025-07-21 21:01:01 +0530

    cors true

--- src/main/java/com/project/ecommerce/config/WebConfig.java
+++ src/main/java/com/project/ecommerce/config/WebConfig.java
@@ -29,6 +29,6 @@ public class WebConfig implements WebMvcConfigurer {
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(false); // false if not using cookies
+                .allowCredentials(true);
     }
 }
