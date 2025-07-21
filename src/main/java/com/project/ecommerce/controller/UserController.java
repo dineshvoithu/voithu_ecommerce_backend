c47790e0f527ac66e8f3ebeecb0cfe1c7ad088c9
dineshvoithu <dineshkumarvoithu@gmail.com>
2025-07-21 20:07:29 +0530

    Fix error

--- src/main/java/com/project/ecommerce/controller/UserController.java
+++ src/main/java/com/project/ecommerce/controller/UserController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "https://voithucart.vercel.app")
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
